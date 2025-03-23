a4755f1162a14681ee7441f7b191d1594a917be8
Vault777 <Vault777.mail@gmail.com>
2025-03-23 12:35:00 +0200

    init upload

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,84 @@
+import { defineConfig } from 'vitepress'
+
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  cleanUrls: true,
+  lang: 'en-US',
+  title: "VAULT 777",
+  description: "VAULT777 Documentation & Help",
+  //lastUpdated: true,
+  locales: {
+    root: {
+      label: 'English',
+      lang: 'en'
+    },
+    zh: {
+      label: '中文',
+      lang: 'zh', // optional, will be added  as `lang` attribute on `html` tag
+      link: '/zh/' // default /fr/ -- shows on navbar translations menu, can be external
+    }
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    search: {
+      provider: 'local'
+    },
+    logo: '/logo.png',
+    
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/General/About.md' },
+      { text: 'Quick Start', link: '/markdown-examples' },
+      { text: 'Examples', link: '/markdown-examples' },
+      {
+        text: 'Games',
+        items: [
+          { text: 'Dice', link: '/Games/House Originals/Dice.md' },
+          { text: 'Plinko', link: '/Games/House Originals/Plinko.md' },
+          { text: 'Slots', link: '/Games/House Originals/Slots.md' },
+          { text: 'Mines', link: '/Games/House Originals/Mines.md' },
+          { text: 'Video Poker', link: '/Games/House Originals/Video Poker.md' },
+          { text: 'Rock Paper Scissors', link: '/Games/House Originals/Rock Paper Scissors.md' },
+          { text: 'Coin Flip', link: '/Games/House Originals/Coin Flip.md' }
+        ]
+      }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Games: House Originals',
+        items: [
+          { text: 'Dice', link: '/Games/House Originals/Dice.md' },
+          { text: 'Plinko', link: '/Games/House Originals/Plinko.md' },
+          { text: 'Slots', link: '/Games/House Originals/Slots.md' },
+          { text: 'Mines', link: '/Games/House Originals/Mines.md' },
+          { text: 'Video Poker', link: '/Games/House Originals/Video Poker.md' },
+          { text: 'Rock Paper Scissors', link: '/Games/House Originals/Rock Paper Scissors.md' },
+          { text: 'Coin Flip', link: '/Games/House Originals/Coin Flip.md' }
+        ]
+      },
+      
+    ],
+
+    footer: {
+      message: 'VAULT777 is a decentralized protocol. Please gamble responsibly and in accordance with your local laws and regulations.',
+      copyright: 'Copyright © 2025 VAULT777. All rights reserved.'
+    },
+
+    socialLinks: [
+      { icon: 'homepage', link: 'https://vault777.com' },
+      { icon: 'github', link: 'https://github.com/Vault777com/dapp' },
+      { icon: 'x', link: 'https://x.com' },
+      { icon: 'discord', link: 'https://discord.gg' },
+      { icon: 'telegram', link: 'https://t.me' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,43 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "VAULT 777 Docs"
+  text: "Documentation"
+  tagline: Play, Stake and Own the Vault The First Fully Decentralized Casino
+  image:
+   src: 'logo.png'
+   alt: 'Logo'
+  actions:
+    - theme: brand
+      text: Quick Start
+      link: /markdown-examples
+    - theme: alt
+      text: Smart Contracts
+      link: /api-examples
+    - theme: alt
+      text: Read on GitHub
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature D
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature E
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature F
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature G
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature H
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature I
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
